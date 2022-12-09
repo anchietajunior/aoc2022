1e697d7ecd732f2510c1c6b153ef9a9057dddf9a
anchietajunior <santosjr87@gmail.com>
2022-12-09 08:42:51 -0300

    Day 01 - Calorie Counting

---
+++ day01.rb
@@ -0,0 +1,27 @@
+
+#data = DATA.readlines
+data = File.readlines(ARGV.first)
+p data
+  .map(&:chomp)
+  .chunk_while{ _2 != "" }
+  .map { _1.map(&:to_i) }
+  .map(&:sum)
+  #.max # Part of the first problem
+  .sort[-3...]
+  .sum
+
+__END__
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
